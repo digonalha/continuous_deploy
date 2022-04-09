cf9e1939964bc64fc6a515e0226833bb433dba0f
Rodrigo Fernandes <digonalha@gmail.com>
2022-04-09 19:36:26 -0300

    init repository

---
+++ git_job.py
@@ -0,0 +1,18 @@
+# test github script to auto deploy project
+import os
+import git as g
+import subprocess
+
+repo_dir = os.getenv("REPOSITORY_DIR")
+file_name = os.getenv("FILENAME")
+file_dir = f"{repo_dir}\\{file_name}"
+
+git = g.cmd.Git(repo_dir)
+msg = git.pull()
+
+print(msg)
+
+if msg != 'Already up to date.':
+    with open("./output.log", "w") as output:
+        subprocess.call(f"docker-compose.exe -f {file_dir} up --build -d", shell=True, stdout=output, stderr=output)
+
